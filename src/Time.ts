69b399c6a317637d52d82c80c98e1f35fb0fdaf1
lao-jiawei <lao-jiawei@qq.com>
2024-06-26 14:38:46 +0800

    feat: 添加时间格式化

--- src/Time.ts
+++ src/Time.ts
@@ -10,7 +10,38 @@ const getWeekDay = (date, prefix = "星期") => {
   return `${prefix}${weekDayText}`;
 }
 
+
+/**
+ * @func 格式化
+ * @desc 
+ * @param {}  
+ * @return {} 
+ */
+function format(date, formatStr = 'YYYY-MM-DD HH:mm:ss') {
+  const curDate = new Date(date);
+  const TIME_STRING_MAP = {
+    "M+": curDate.getMonth() + 1,                 //月份
+    "D+": curDate.getDate(),                    //日
+    "h+": curDate.getHours(),                   //小时
+    "H+": curDate.getHours(),                   //小时
+    "m+": curDate.getMinutes(),                 //分
+    "s+": curDate.getSeconds(),                 //秒
+    "q+": Math.floor((curDate.getMonth() + 3) / 3), //季度
+    "S": curDate.getMilliseconds()             //毫秒
+  };
+  if (/(Y+)/.test(formatStr) || /(y+)/.test(formatStr)) {
+    formatStr = formatStr.replace(RegExp.$1, (curDate.getFullYear() + "").substr(4 - RegExp.$1.length));
+  }
+  for (var k in TIME_STRING_MAP) {
+    if (new RegExp("(" + k + ")").test(formatStr)) {
+      formatStr = formatStr.replace(RegExp.$1, (RegExp.$1.length == 1) ? (TIME_STRING_MAP[k]) : (("00" + TIME_STRING_MAP[k]).substr(("" + TIME_STRING_MAP[k]).length)));
+    }
+  }
+  return formatStr;
+}
+
 const Time = {
+  format,
   getWeekDay,
 }
 
--- test/Time.test.js
+++ test/Time.test.js
@@ -2,6 +2,13 @@ import { Time } from "../src";
 test('测试Time.getWeekDay', () => {
   expect(Time.getWeekDay('2024-06-20')).toBe('星期四');
 
-  expect(Time.getWeekDay('2024-06-20','周')).toBe('周四');
+  expect(Time.getWeekDay('2024-06-20', '周')).toBe('周四');
 
 })
+
+test('测试Time.format', () => {
+  expect(Time.format('2024-06-20', 'YYYY-MM-DD')).toBe('2024-06-20');
+  expect(Time.format('2024-06-20', 'yyyy-MM-DD')).toBe('2024-06-20');
+})
+
+
