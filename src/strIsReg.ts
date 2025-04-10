ea7200df4aaaf11089126792c6a36d02bb4a32da
Jv <lao-jiawei@qq.com>
2025-04-10 17:11:26 +0800

    fix: 修复发布失败

--- src/strIsReg.ts
+++ src/strIsReg.ts
@@ -1,8 +1,17 @@
-export function strIsReg(str: string) {
+/**
+ * @func 判断字符串是否是正则表达式
+ * @desc 
+ * @param {}  
+ * @return {} 
+ */
+function strIsReg(str) {
+  let isReg = false;
   try {
-    new RegExp(str);
-    return true;
-  } catch {
-    return false;
+    isReg = eval(str) instanceof RegExp
+  } catch (e) {
+    isReg = false
   }
+  return isReg
 }
+
+export default strIsReg;
