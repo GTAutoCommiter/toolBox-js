aa6a1a1ab501b7fba50c967ccfa7b9d6675fbc42
Jv <lao-jiawei@qq.com>
2025-09-05 17:30:24 +0800

    fix: 修复git-action报错

--- src/groupByValue.ts
+++ src/groupByValue.ts
@@ -1,14 +1,18 @@
 /**
  * 根据对象的值对对象的键进行分组。
- * @param {object} obj The object to group.
- * @returns {object} 一个以原对象的value为键，原对象的key组成的数组为值的对象。
+ * @param obj The object to group.
+ * @returns 一个以原对象的value为键，原对象的key组成的数组为值的对象。
  */
-function groupByValue(obj) {
-  const res = Object.entries(obj).reduce((acc, [key, val]) => {
-    acc[val] = acc[val] || [];
-    acc[val].push(key);
-    return acc;
-  }, {});
-  return res;
+function groupByValue(obj: Record<string, any>): Record<string, string[]> {
+  return Object.entries(obj).reduce(
+    (acc: Record<string, string[]>, [key, val]) => {
+      const valueKey = String(val);
+      acc[valueKey] = acc[valueKey] || [];
+      acc[valueKey].push(key);
+      return acc;
+    },
+    {},
+  );
 }
 export default groupByValue;
+
